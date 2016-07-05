5aed44fd5c0d61a8644daaa2c5281b71cb2b035c
David V. Wallin <david@dwall.in>
2016-07-06 01:24:23 +0200

    GetJSON and GetText works now

--- main.go
+++ main.go
@@ -3,18 +3,39 @@ package konfettio
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func GetJSONFromUrl(user_id int, pool_slug string, hash_key string) string {
-	url := fmt.Sprintf("https://konfett.io/c/%d/%s/%s/json", user_id, pool_slug, hash_key)
+type Response struct {
+	Key   string `json:"Key"`
+	Value string `json:"Value"`
+}
+
+func GetJSON(user_id int, pool_slug string, hash_key string) (target Response) {
+	url := fmt.Sprintf("https://konfett.io/c/%d/%s/%s/jsonverbose", user_id, pool_slug, hash_key)
+	fmt.Println(url)
 	r, err := http.Get(url)
 	if err != nil {
 		log.Printf("error getting value from url: %s\n", err.Error())
 	}
 	defer r.Body.Close()
-	var target string
-	json.NewDecoder(r.Body).Decode(target)
+	json.NewDecoder(r.Body).Decode(&target)
 	return target
+}
+
+func GetText(user_id int, pool_slug string, hash_key string) (target string) {
+	url := fmt.Sprintf("https://konfett.io/c/%d/%s/%s/text", user_id, pool_slug, hash_key)
+	fmt.Println(url)
+	r, err := http.Get(url)
+	if err != nil {
+		log.Printf("error getting value from url: %s\n", err.Error())
+	}
+	defer r.Body.Close()
+	bs, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("error reading value from url: %s\n", err.Error())
+	}
+	return string(bs)
 }
